4e7ddd9351bf3e9484d6d63e6018ae3cbc5c4ce2
ingEmprende <silviojsoto@gmail.com>
2018-07-05 16:28:22 +0000

    Se añadio Materialize

---
+++ resources/views/index.blade.php
@@ -0,0 +1,58 @@
+@extends('Marco.Principal')
+
+@section('content')
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="section"></div>
+    <main>
+    <center>
+
+      <div class="container">
+        <div class="z-depth-1 grey lighten-4 row" style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
+        <i class=" large material-icons">account_circle</i>
+            <div class='row'>
+              <div class='col s12'>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='input-field col s12'>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='input-field col s12'>
+              </div>
+              <label style='float: right;'>				
+			</label>
+            </div>
+            <br />
+            <center>
+              <div class='row'>
+              </div>
+            </center>
+        </div>
+      </div>
+    </center>
+ 
+        </main>
+</div>
+
+  <footer class="page-footer color-lateral" >
+      <div class="footer-copyright">
+        <div class="container">
+
+        <a class="grey-text text-lighten-4 right" href="#!">Mas Información</a>
+        </div>
+      </div>
+    </footer>
+@stop
